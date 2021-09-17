0d04e6792deb84801d4bee6e35529ca21512d29f
Pratim Sarangi <pratim@pratims>
2021-09-17 21:33:45 +0530

    Improved code documentation

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import v1Routes from './v1';
 
 const router = express.Router();
 
+// API versioning
 router.use('/v1', v1Routes);
 
 export default router;
--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -3,6 +3,7 @@ import recordsRoutes from './records/routes';
 
 const router = express.Router();
 
+// All the V1 APIs goes here
 router.use('/records', recordsRoutes);
 
 export default router;
